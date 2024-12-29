840a776b0da6fe14fdce910a91ea15ba63e4d063
Nacho Gómez <terminatitorxdyt@gmail.com>
2024-12-29 07:28:18 -0500

    implementing new project and creating initial states and changing turn functionality

--- projects/02-tic-tac-toe/README.md
+++ projects/02-tic-tac-toe/README.md
@@ -1,3 +1,3 @@
 # React + Vite
 
-This mini project was created to apply the useEffect in a Tic Tac Toe application
+This mini project was created to apply the useEffect in a Tic Tac Toe application, saving the game status in the localStorage as data persistence.
--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,13 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '../../../../../../../vite.svg'
-import './App.css'
+import { useState } from "react"
 
-function App () {
-  const [count, setCount] = useState(0)
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+
+const Square = ({ children, isSelected, updateBoard, index}) => {
+  const className = `square ${isSelected ? 'is-selected': ''}`
+  const handleClick = () => {
+    updateBoard(index)
+  }
+  return(
+    <div className={className} onClick={handleClick}>
+      {children}
+    </div>
+  )
+} 
+
+function App() {
+  // Initial board state
+  const [board, setBoard] = useState(Array(9).fill(null))
+  // Initial player turn
+  const [turn, setTurn] = useState(TURNS.X)
+  // Winner State
+  const [winner, setWinner] = useState(null)
+
+  const updateBoard = (index) => {
+    if(board[index]) return
+    
+    const newBoard = [...board]
+    newBoard[index] = turn    
+    setBoard(newBoard)    
+
+    // Changing Turns between players
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    
+
+  } 
 
   return (
-    <h1>Hola mundo</h1>
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {
+          board.map( (_, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+      <section className="turn">
+        <Square isSelected={turn == TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn == TURNS.O}>{TURNS.O}</Square>
+      </section>
+    </main>
   )
 }
 
