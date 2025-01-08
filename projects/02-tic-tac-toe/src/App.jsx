42dcd60c0e1b66f75b45036ca44d695f4ffcc8ec
Nacho Gómez <terminatitorxdyt@gmail.com>
2025-01-08 11:58:48 -0500

    feat(App): add error handling for localStorage access and improve game state management

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -10,17 +10,32 @@ import { resetStoredGame, storeGame } from './logic/storage'
 function App () {
   // Initial board state
   const [board, setBoard] = useState(() => {
-    const storedBoard = window.localStorage.getItem('board')
-    return storedBoard ? JSON.parse(storedBoard) : Array(9).fill(null)
+    try {
+      const storedBoard = window.localStorage.getItem('board')
+      return storedBoard ? JSON.parse(storedBoard) : Array(9).fill(null)
+    } catch (error) {
+      console.error('Error parsing board from localStorage:', error)
+      return Array(9).fill(null) // Default empty board
+    }
   })
+
   // Initial player turn
   const [turn, setTurn] = useState(() => {
-    const storedTurn = window.localStorage.getItem('turn')
-    return storedTurn ?? TURNS.X
+    try {
+      const storedTurn = window.localStorage.getItem('turn')
+      return storedTurn && (storedTurn === TURNS.X || storedTurn === TURNS.O)
+        ? storedTurn
+        : TURNS.X
+    } catch (error) {
+      console.error('Error parsing turn from localStorage:', error)
+      return TURNS.X // Default starting turn
+    }
   })
+
   // Winner State
   const [winner, setWinner] = useState(null)
 
+  // Reset the game state
   const resetGame = () => {
     resetStoredGame()
     setBoard(Array(9).fill(null))
@@ -28,6 +43,7 @@ function App () {
     setTurn(TURNS.X)
   }
 
+  // Update board and handle game logic
   const updateBoard = (index) => {
     if (board[index] || winner) return
 
@@ -46,20 +62,18 @@ function App () {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
 
-    storeGame(newTurn, newBoard)
+    storeGame({ newBoard, newTurn })
   }
 
   return (
     <main className='board'>
       <h1>Tic Tac Toe</h1>
-      <button className='resetGame' onClick={resetGame}>Reset Game</button>
+      <button className='resetGame' onClick={resetGame}>
+        Reset Game
+      </button>
       <section className='game'>
         {board.map((square, index) => (
-          <Square
-            key={index}
-            index={index}
-            updateBoard={updateBoard}
-          >
+          <Square key={index} index={index} updateBoard={updateBoard}>
             {square}
           </Square>
         ))}
