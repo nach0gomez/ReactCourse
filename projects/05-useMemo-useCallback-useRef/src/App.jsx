e929975a3678383d130e4dc1e8b3801bd350f3a2
Nacho Gómez <terminatitorxdyt@gmail.com>
2025-01-10 01:19:21 -0500

    feat(movies): add movie search functionality with mock data and error handling

--- projects/05-useMemo-useCallback-useRef/src/App.jsx
+++ projects/05-useMemo-useCallback-useRef/src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+// import mockResponse from './mocks/results.json'
+import mockNoResponse from './mocks/no-results.json'
+import { Movies } from './components/Movies'
+import { useMovies } from './hooks/useMovies'
 
 function App () {
-  return (
-    <>
+  const { movies: mappedMovies } = useMovies()
 
-    </>
+  const handleSubmit = (event) => {
+    // TODO: check form validation and access
+    console.log(event.value)
+    event.preventDefault()
+  }
+
+  return (
+    <Movies movies={mappedMovies} handleSubmit={handleSubmit} />
   )
 }
 
---
+++ projects/05-useMemo-useCallback-useRef/src/hooks/useMovies.js
@@ -0,0 +1,14 @@
+import mockResponse from '../mocks/results.json'
+
+export function useMovies () {
+  const movies = mockResponse.Search
+
+  const mappedMovies = movies?.map(movie => ({
+    id: movie.imdbID,
+    title: movie.Title,
+    year: movie.Year,
+    poster: movie.Poster
+  }))
+
+  return { movies: mappedMovies }
+}
