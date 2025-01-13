86dcf4c51de5c5c9553c1a1ee58378bb14ed7c12
Nacho Gómez <terminatitorxdyt@gmail.com>
2025-01-13 16:48:45 -0500

    feat(App): implement form validation for movie title input and update Movies component structure

--- projects/05-useMemo-useCallback-useRef/src/App.jsx
+++ projects/05-useMemo-useCallback-useRef/src/App.jsx
@@ -3,18 +3,56 @@ import './App.css'
 import mockNoResponse from './mocks/no-results.json'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
+import { useEffect, useState } from 'react'
 
 function App () {
   const { movies: mappedMovies } = useMovies()
+  const [title, setTitle] = useState('')
+  const [error, setError] = useState('')
 
   const handleSubmit = (event) => {
-    // TODO: check form validation and access
-    console.log(event.value)
     event.preventDefault()
+    const { title } = Object.fromEntries(
+      new window.FormData(event.target)
+    )
+    console.log(title)
   }
 
+  const handleChange = (event) => {
+    setTitle(event.target.value)
+  }
+
+  useEffect(() => {
+    if (title === '') {
+      setError('Title can not be empty')
+      return
+    }
+
+    if (title.match(/^\d+$/)) {
+      setError('Title can not be a digit')
+      return
+    }
+
+    if (title.length < 3) {
+      setError('Title should be at least 3 characters long')
+      return
+    }
+
+    setError('')
+  }, [title])
+
   return (
-    <Movies movies={mappedMovies} handleSubmit={handleSubmit} />
+    <div className='index'>
+      <header>
+        <h2>Movie Finder</h2>
+        <form className='form' onSubmit={handleSubmit}>
+          <input value={title} onChange={handleChange} id='input' name='title' type='text' placeholder='Avengers, StarWars, Matrix...' />
+          <button type='submit'>Search</button>
+          {error && (<p style={{ color: 'red' }}>{error}</p>)}
+        </form>
+      </header>
+      <Movies onChange={handleChange} value={title} movies={mappedMovies} handleSubmit={handleSubmit} />
+    </div>
   )
 }
 
--- projects/05-useMemo-useCallback-useRef/src/components/Movies.jsx
+++ projects/05-useMemo-useCallback-useRef/src/components/Movies.jsx
@@ -1,29 +1,21 @@
 /* eslint-disable react/prop-types */
 
-export const Movies = ({ movies, handleSubmit }) => {
+export const Movies = ({ movies }) => {
   const hasMovies = movies.length > 0
   return (hasMovies
     ? (
-      <div className='index'>
-        <header>
-          <h2>Movie Finder</h2>
-          <form className='form' onSubmit={handleSubmit}>
-            <input id='input' type='text' placeholder='Avengers, StarWars, Matrix...' />
-            <button type='submit'>Search</button>
-          </form>
-        </header>
-        <main>
-          {movies.map(movie => {
-            return (
-              <article key={movie.id}>
-                <label>{movie.title}</label>
-                <label>{movie.year}</label>
-                <img src={movie.poster} alt={movie.title} />
-              </article>
-            )
-          })}
-        </main>
-      </div>)
+      <main>
+        {movies.map(movie => {
+          return (
+            <article key={movie.id}>
+              <label>{movie.title}</label>
+              <label>{movie.year}</label>
+              <img src={movie.poster} alt={movie.title} />
+            </article>
+          )
+        })}
+      </main>
+      )
     : <p>There are no movies found.</p>
   )
 }
