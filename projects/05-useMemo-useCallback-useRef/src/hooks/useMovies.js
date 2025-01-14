63c1b4a9cbf96c986bf8ad992f413378f1a780de
Nacho Gómez <terminatitorxdyt@gmail.com>
2025-01-14 04:59:25 -0500

    feat(hooks): optimize useMovies to prevent unnecessary API calls on repeated searches

--- projects/05-useMemo-useCallback-useRef/src/hooks/useMovies.js
+++ projects/05-useMemo-useCallback-useRef/src/hooks/useMovies.js
@@ -1,15 +1,19 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { searchMovies } from '../services/movies'
 
 export function useMovies ({ title }) {
   const [responseMovies, setResponseMovies] = useState([])
   const [loading, setLoading] = useState(false)
   const [fetchError, setFetchError] = useState(null)
+  const prevSearch = useRef(title)
 
   const getMovies = async () => {
     try {
+      if (prevSearch.current === title) return
+
       setLoading(true)
       setFetchError(null)
+      prevSearch.current = title
       const mappedMovies = await searchMovies({ title })
       setResponseMovies(mappedMovies)
     } catch (e) {
